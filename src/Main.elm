4d9611be1fbedd92f5b8e6f46cd5eaa5b30f02e9
Yuri Guilherme <yurigm97@hotmail.com>
2020-03-02 21:37:19 -0300

    style(conversion-form): Add style and layout

--- src/Main.elm
+++ src/Main.elm
@@ -1,56 +1,50 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (value, type_, class)
 
+selectClasses =
+  "block appearance-none w-full border shadow py-2 px-3 pr-8 rounded"
 
----- MODEL ----
-
-
-type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
-
-
-
----- PROGRAM ----
-
-
-main : Program () Model Msg
 main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+    div [ class "flex justify-center py-10" ]
+        [ div [ class "w-full max-w-xs" ]
+            [ h1 [ class "text-center text-2xl mb-6" ] [ text "Conversor de Moedas" ]
+            , form [ class "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" ]
+                [ div [ class "mb-4" ]
+                    [ label [ class "block text-gray-700 text-sm font-bold mb-2" ]
+                        [ text "Moeda de origem" ]
+                    , div [ class "relative" ]
+                        [ select
+                            [ class selectClasses ]
+                            [ option [ value "BRL"] [ text "Real" ] 
+                            , option [ value "USD"] [ text "Dólar americano" ]
+                            , option [ value "EUR"] [ text "Euro" ] 
+                            ]
+                        ]
+                    ]
+                , div [ class "mb-4" ]
+                    [ label [ class "block text-gray-700 text-sm font-bold mb-2" ]
+                        [ text "Moeda de destino"
+                        ]
+                    , div [ class "relative" ]
+                        [ select
+                            [ class "block appearance-none w-full border shadow py-2 px-3 pr-8 rounded" ]
+                            [ option [ value "USD" ] [ text "Dólar americano" ]
+                            , option [ value "BRL" ] [ text "Real" ] 
+                            , option [ value "EUR" ] [ text "Euro" ] 
+                            ]
+                        ]
+                    ]
+                , div [ class "mb-6" ]
+                    [ label [ class "block text-gray-700 text-sm font-bold mb-2" ]
+                        [ text "Quantidade"
+                        ]
+                    , input [ type_ "number", class "shadow appearence-none border rounded w-full py-2 px-3 text-gray" ] []
+                    ]
+                , div [ class "flex w-full" ]
+                    [ button [ class "bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4" ] [ text "Converter" ] ]
+                ]
+            ]
+        ]
