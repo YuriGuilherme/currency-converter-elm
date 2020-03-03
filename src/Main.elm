a79773a04ad05a408f9c42d246827275fc4a84ce
Yuri Guilherme <yurigm97@hotmail.com>
2020-03-03 01:09:05 -0300

    feat(currency-converter): Add update function

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,38 @@
 module Main exposing (main)
 
 import Browser
+import Dict
 import Html exposing (..)
 import Html.Attributes exposing (value, type_, class, selected)
+import Html.Events exposing (onInput, onSubmit)
 
 selectClasses =
   "block appearance-none w-full border shadow py-3 px-3 pr-8 rounded"
+
+brl =
+    Dict.fromList
+        [ ( "EUR", 0.21 )
+        , ( "USD", 0.23 )
+        ]
+
+usd =
+    Dict.fromList
+        [ ( "EUR", 0.92 )
+        , ( "BRL", 4.42 )
+        ]
+
+eur =
+    Dict.fromList
+        [ ( "USD", 1.09 )
+        , ( "BRL", 4.81 )
+        ]
+
+currencies =
+    Dict.fromList
+        [ ( "BRL", brl )
+        , ( "EUR", eur )
+        , ( "USD", usd )
+        ]
 
 type alias Model =
     { from : String
@@ -14,7 +41,11 @@ type alias Model =
     , result : Float
     }
 
-type Msg = Nothing 
+type Msg
+    = ChangeOriginCurrency String
+    | ChangeDestinyCurrency String
+    | ChangeAmount String
+    | SubmitForm
 
 initialModel : Model
 initialModel =
@@ -26,20 +57,42 @@ initialModel =
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        ChangeOriginCurrency currencyCode ->
+            { model | from = currencyCode }
+        ChangeDestinyCurrency currencyCode ->
+            { model | to = currencyCode }
+        ChangeAmount amount ->
+            case String.toFloat amount of
+              Just value ->
+                  { model | amount = value }
+              Nothing ->
+                  model
+        SubmitForm ->
+            let
+                availableCurrencies =
+                    Maybe.withDefault Dict.empty (Dict.get model.from currencies)
+
+                destinyCurrencyValue =
+                    Maybe.withDefault 1 (Dict.get model.to availableCurrencies)
+
+                result =
+                    destinyCurrencyValue * model.amount
+            in
+                { model | result = result }
 
 view : Model -> Html Msg
 view model =
     div [ class "flex justify-center py-10" ]
         [ div [ class "w-full max-w-xs" ]
             [ h1 [ class "text-center text-2xl mb-6" ] [ text "Conversor de Moedas" ]
-            , form [ class "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" ]
+            , form [ onSubmit SubmitForm, class "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" ]
                 [ div [ class "mb-4" ]
                     [ label [ class "block text-gray-700 text-sm font-bold mb-2" ]
                         [ text "Moeda de origem" ]
                     , div [ class "relative" ]
                         [ select
-                            [ class selectClasses, value model.from ]
+                            [ class selectClasses, value model.from, onInput ChangeOriginCurrency ]
                             [ option [ value "BRL", selected (model.from == "BRL") ] [ text "Real" ] 
                             , option [ value "USD",selected (model.from == "USD") ] [ text "Dólar americano" ]
                             , option [ value "EUR", selected (model.from == "EUR") ] [ text "Euro" ] 
@@ -52,7 +105,7 @@ view model =
                         ]
                     , div [ class "relative" ]
                         [ select
-                            [ class selectClasses, value model.to ]
+                            [ class selectClasses, value model.to, onInput ChangeDestinyCurrency ]
                             [ option [ value "USD", selected (model.to == "USD") ] [ text "Dólar americano" ]
                             , option [ value "BRL", selected (model.to == "BRL") ] [ text "Real" ] 
                             , option [ value "EUR", selected (model.to == "EUR") ] [ text "Euro" ] 
@@ -63,7 +116,7 @@ view model =
                     [ label [ class "block text-gray-700 text-sm font-bold mb-2" ]
                         [ text "Quantidade"
                         ]
-                    , input [ type_ "number", value (String.fromFloat model.amount), class "shadow appearence-none border rounded w-full py-2 px-3 text-gray" ] []
+                    , input [ type_ "number", onInput ChangeAmount, value (String.fromFloat model.amount), class "shadow appearence-none border rounded w-full py-2 px-3 text-gray" ] []
                     ]
                 , div [ class "flex w-full" ]
                     [ button [ class "bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4" ] [ text "Converter" ] ]
